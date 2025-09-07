7b46a474e4dd2fd92415df41629686a6e86186c4
marianatalos52-web <marianatalos52@gmail.com>
2025-09-07 15:19:07 +0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Călătorii cu suflet în Turcia</h1>
-      <p>Bine ai venit pe blogul nostru spiritual despre călătoriile în Turcia!</p>
-    </main>
-  );
-}
