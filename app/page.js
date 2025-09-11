8584f184f09c8b8f162feee181829a57beb91099
marianatalos52-web <marianatalos52@gmail.com>
2025-09-11 14:38:23 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Bine ai venit la Călătorii cu suflet în Turcia</h1>
+      <p>
+        Descoperă locuri sacre,
 
